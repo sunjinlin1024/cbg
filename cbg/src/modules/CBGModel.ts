c9d720cc473d2a32b4dce5fe3107a3fb6a91185a
lynn <272295408@qq.com>
2018-11-09 21:37:36 +0800

    update

--- cbg/src/modules/CBGModel.ts
+++ cbg/src/modules/CBGModel.ts
@@ -12,7 +12,7 @@ class CBGModel extends BaseModel{
 			let price=Math.floor(item.price_int*0.01)
 			console.log(stringUtil.expandStr(item.equip_name,12)+"\t",
 				"评估值:"+stringUtil.expandStr(evaVal+"",7)+"\t- 售价:"+stringUtil.expandStr(price+"",7)
-				+"\t= 利润:"+stringUtil.expandStr(String(evaVal-price),8)+"\t 利润比:"+Math.floor((evaVal-price)/price*100)+"%)")
+				+"\t= 利润:"+stringUtil.expandStr(String(evaVal-price),8)+"\t 利润比:"+Math.floor((evaVal-price)/price*100)+"%")
 		}
 	}
 }
