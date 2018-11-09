e2ecaef6df795715bfcb607380cd3e5ce24a5e17
lynn <272295408@qq.com>
2018-11-09 21:19:16 +0800

    update

--- cbg/src/modules/CBGModel.ts
+++ cbg/src/modules/CBGModel.ts
@@ -8,7 +8,7 @@ class CBGModel extends BaseModel{
 			item=modelFactory.createModelByKindid(data[Const.KEY_KINDID])
 			item.initByData(data)
 			this._shopList[item.equipid]=item
-			console.log(item.equip_name+(new Array(12-item.equip_name.length).join(" ")),"评估值："+config.CoinToRMB(item.evaluateValue,item.serverid)+"元","\t售价："+Math.floor(item.price_int*0.01)+"元")
+			console.log(item.equip_name+(new Array(12-item.equip_name.length).join(" ")),"\t评估值："+config.CoinToRMB(item.evaluateValue,item.serverid)+"元","\t售价："+Math.floor(item.price_int*0.01)+"元")
 		}
 	}
 }
